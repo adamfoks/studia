b40575af4949a9bf058a459a57c2df51438841a0
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-03-14 20:57:23 +0100

    webpack config

---
+++ webpack/lekcja01/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { title } = require('process');
+
+module.exports = {
+    plugins:[
+        new HtmlWebpackPlugin(
+            hash: true,
+            title: 
+        )
+    ]
+};
