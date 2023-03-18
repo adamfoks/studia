95b5372d9d042822b03fe27135b250a9b577e9b7
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-03-18 16:52:25 +0100

    react tasks

---
+++ react1/konfiguracja/src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function App() {
+    return (
+        <>
+            <h1>Siema świecie!</h1>
+            <h2>Tip: Check your console</h2>
+        </>
+    );
+}
--- react1/konfiguracja/src/index.js
+++ react1/konfiguracja/src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello!");
+import React from "react";
+import ReactDOM  from "react-dom";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App/>,root);
